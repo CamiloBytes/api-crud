5ecaa158ab26a5af10af78cecbbbc17c00865061
Camilo Parra <camiloandres0222@gmail.com>
2026-01-29 07:54:01 -0500

    Refactor UserController index method to return JSON response of all users

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     // muestra la lista de usuarios
     public function index()
     {
-        return "obteniendo lista de usuarios desde el controlador.";
+        $users = User::all();
+        return response()->json($users);
     }
 }
 
